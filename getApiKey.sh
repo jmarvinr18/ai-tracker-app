51d673a433d210df4e74f3245787adf8bde33d01
jmarvinr18 <jmarvin.ramoda@gmail.com>
2026-08-26 03:31:46 +0000

    update data

--- getApiKey.sh
+++ getApiKey.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws lambda invoke --function-name aiteam-prmj-tracker-bootstrap \
---payload '{"action": "register_agent", "agent_id": "slgs-ai-agent", "threshold_minutes": 2 }' \
+--payload '{"action": "register_agent", "agent_id": "clarvo-rag-agent", "threshold_minutes": 2 }' \
 --output table \
  --region us-east-1 response.json \
  --debug
